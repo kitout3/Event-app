01524d8c1bac2811bb47e4f56ed0a6eb4edfdeb7
kitout3 <sella.quentin@gmail.com>
2026-09-19 08:15:48 +0200

    feat: add standalone software admin bootstrap

---
+++ src/software-admin-main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SoftwareAdmin from "./SoftwareAdmin.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode><SoftwareAdmin /></React.StrictMode>
+);
