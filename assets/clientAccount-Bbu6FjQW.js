1b7674f770914a60fb86258d650e392d866d92dd
kitout3 <kitout3@users.noreply.github.com>
2026-09-20 18:48:32 +0000

    deploy: 8e7d6955f5ec1d08d1487436b016dbccfb16517a

---
+++ assets/clientAccount-Bbu6FjQW.js
@@ -0,0 +1,1 @@
+import{c as e,j as t,R as o}from"./PasswordInput-CPuYnU8i.js";import{C as r}from"./account-Coc0jQHJ.js";e.createRoot(document.getElementById("root")).render(t.jsx(o.StrictMode,{children:t.jsx(r,{})}));
