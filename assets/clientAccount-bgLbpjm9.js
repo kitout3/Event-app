d2b051a469d3cb096ab3906b01bdc27450e0befa
kitout3 <kitout3@users.noreply.github.com>
2026-09-20 17:21:44 +0000

    deploy: ac41e794f31ddb9c801cdc04b0211439a718ae87

--- assets/clientAccount-bgLbpjm9.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as t,R as o}from"./billing-config-BjIq59RE.js";import{C as r}from"./account-C-bI6xzT.js";e.createRoot(document.getElementById("root")).render(t.jsx(o.StrictMode,{children:t.jsx(r,{})}));
